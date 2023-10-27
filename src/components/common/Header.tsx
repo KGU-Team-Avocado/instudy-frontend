874ca0bf1ca765975c753fb8ea4406d821688df3
hido <bestdohee@gmail.com>
2023-10-27 23:54:22 +0900

    feat: 구글 로그인

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
@@ -6,6 +6,9 @@ import IconButton from '@mui/material/IconButton';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import styled from 'styled-components';
 import { Button } from '@mui/material';
+import {
+  GoogleLogin, GoogleLoginResponse, GoogleLoginResponseOffline, GoogleLogout
+} from 'react-google-login';
 
 // StyledButton 컴포넌트를 생성하여 남색 스타일 적용
 const StyledButton = styled(Button)`
@@ -15,7 +18,32 @@ const StyledButton = styled(Button)`
   }
 `;
 
+// interface GoogleResponse {
+//   profileObj: {
+//     email: string;
+//     familyName: string;
+//     givenName: string;
+//     googleId: string;
+//     imageUrl: string;
+//     name: string;
+//   };
+//   tokenId: string;
+// }
+
 function Header() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const responseGoogle = (response: GoogleLoginResponse | GoogleLoginResponseOffline) => {
+    if ('profileObj' in response) {
+      setIsLoggedIn(true);
+      console.log(response);
+    }
+  };
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+  };
+
   return (
     <AppBar position="fixed" color="default">
       <Toolbar>
@@ -25,6 +53,21 @@ function Header() {
         </Typography>
         {/* StyledButton 컴포넌트 사용 */}
         <StyledButton variant="contained">프리미엄</StyledButton>
+        {isLoggedIn ? (
+          <GoogleLogout
+            clientId="873237699798-mn38k7vuo7idkp4p4cbotj80v1taf814.apps.googleusercontent.com"
+            buttonText="로그아웃"
+            onLogoutSuccess={handleLogout}
+          />
+        ) : (
+          <GoogleLogin
+            clientId="873237699798-mn38k7vuo7idkp4p4cbotj80v1taf814.apps.googleusercontent.com"
+            buttonText="Google 로그인"
+            onSuccess={responseGoogle}
+            onFailure={responseGoogle}
+            cookiePolicy="single_host_origin"
+          />
+        )}
         <IconButton color="inherit">
           <NotificationsIcon />
         </IconButton>
